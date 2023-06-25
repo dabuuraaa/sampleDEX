726b92ad3555257d8345a7922b42c98c0be1d0af
dabuuraaa <ryotugboat@gmail.com>
2023-06-25 12:21:40 +0700

    Create SdexFactory.sol

---
+++ contracts/SdexFactory.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import './SdexPool.sol';
+
+contract SdexFactory {
+    mapping(address => mapping(address => address)) public getPool;
+
+    event PoolCreated(address indexed token0, address indexed token1, address pool);
+
+    function createPool(address tokenA, address tokenB) external returns (address pool) {
+        require(tokenA != tokenB, 'SdexFactory: IDENTICAL_TOKEN_ADDRESS');
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), 'SdexFactory: ZERO_ADDRESS');
+        require(getPool[token0][token1] == address(0), 'SdexFactory: TOKEN_POOL_EXISTS');
+
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+        SdexPool poolContract = new SdexPool{salt: salt}();
+        poolContract.initialize(token0, token1);
+
+        pool = address(poolContract);
+        getPool[token0][token1] = pool;
+        getPool[token1][token0] = pool;
+        emit PoolCreated(token0, token1, pool);
+    }
+}
